36506f04c565261d0f6fc9ac9804ca34b5311703
dudiq <idudiq@gmail.com>
2013-10-20 22:21:35 +0400

    change names in index

--- run.js
+++ run.js
@@ -3,13 +3,13 @@ define(function (require) {
 
         var simplePicker = require("simple-css-cp");
 
-        var elPicker = new simplePicker($("#elPicker"), {
+        var pickerInstance = new simplePicker($("#elPicker"), {
             //cssTheme: "simple-css-cp-small",
             //showButtons: false,
             showInput: true,
             //showPreview: false,
             onChange: function(){
-                log("color", elPicker.getColor());
+                log("color", pickerInstance.getColor());
             }}
         );
 
@@ -21,23 +21,23 @@ define(function (require) {
             console.log(output);
         }
         
-        elPicker.show("hsl(10, 40%, 50%)");
+        pickerInstance.show("hsl(10, 40%, 50%)");
         //hsl(10, 40%, 50%) = rgb(179, 94, 77)
-        log("str2rgba red: ", elPicker.str2rgba("red"));
-        log("str2rgba #fff:", elPicker.str2rgba("#fff"));
-        log("hsv2rgb 10, 40, 20:", elPicker.hsv2rgb(10, 40, 20));
-        log("hsl2rgb 10, 40, 20:", elPicker.hsl2rgb(10, 40, 20));
-        log("rgb2hsv 10, 40, 20:", elPicker.rgb2hsv(10, 40, 20));
-        log("rgb2hsl 10, 40, 20:", elPicker.rgb2hsl(10, 40, 20));
-        log("rgb2hex 10, 40, 20:", elPicker.rgb2hex(10, 40, 20));
-        log("str2hex yellow:", elPicker.str2hex("yellow"));
-        log("str2hex rgb(10,20,30):", elPicker.str2hex("rgb(10,20,30)"));
-        log("hex2rgb #fff:", elPicker.hex2rgb("#fff"));
-        log("hex2rgb #00ff00:", elPicker.hex2rgb("#00ff00"));
+        log("str2rgba red: ", pickerInstance.str2rgba("red"));
+        log("str2rgba #fff:", pickerInstance.str2rgba("#fff"));
+        log("hsv2rgb 10, 40, 20:", pickerInstance.hsv2rgb(10, 40, 20));
+        log("hsl2rgb 10, 40, 20:", pickerInstance.hsl2rgb(10, 40, 20));
+        log("rgb2hsv 10, 40, 20:", pickerInstance.rgb2hsv(10, 40, 20));
+        log("rgb2hsl 10, 40, 20:", pickerInstance.rgb2hsl(10, 40, 20));
+        log("rgb2hex 10, 40, 20:", pickerInstance.rgb2hex(10, 40, 20));
+        log("str2hex yellow:", pickerInstance.str2hex("yellow"));
+        log("str2hex rgb(10,20,30):", pickerInstance.str2hex("rgb(10,20,30)"));
+        log("hex2rgb #fff:", pickerInstance.hex2rgb("#fff"));
+        log("hex2rgb #00ff00:", pickerInstance.hex2rgb("#00ff00"));
 
-//        elPicker.setColor("blue");
-//        elPicker.setColor("black");
-//        elPicker.setColor("white");
-//        elPicker.setColor("rgba(127,127,127,0.3)");
+//        pickerInstance.setColor("blue");
+//        pickerInstance.setColor("black");
+//        pickerInstance.setColor("white");
+//        pickerInstance.setColor("rgba(127,127,127,0.3)");
     });
 });
